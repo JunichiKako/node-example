ac561462e8dcd14566e1f92f6cf12cfc30f89610
Junichi Kako <cjaucono@gmail.com>
2025-07-22 23:51:45 +0900

    install: express.js

--- index.ts
+++ index.ts
@@ -1,11 +1,24 @@
-import * as http from 'http'
+import express from 'express';
 
-const server = http.createServer((re4q,res) => {
-  res.writeHead(200,{'content-type':'text/plain; charset=utf-8'})
-  res.end('Hello Node.js')
-})
-
+const app = express();
 const PORT = 8888;
-server.listen(PORT, () => {
+
+app.get('/', (req, res) => {
+  res.send('Hello web server');
+});
+
+app.listen(PORT, () => {
   console.log('サーバーが起動しました');
-})
+});
+
+// import * as http from 'http'
+
+// const server = http.createServer((re4q,res) => {
+//   res.writeHead(200,{'content-type':'text/plain; charset=utf-8'})
+//   res.end('Hello Node.js')
+// })
+
+// const PORT = 8888;
+// server.listen(PORT, () => {
+//   console.log('サーバーが起動しました');
+// })
